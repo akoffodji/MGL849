2de9fe2846faa2492b325a928d123111a6df8925
Mathieu Yargeau <A_Silversword@hotmail.com>
2018-02-25 17:40:41 -0500

    Desired temp

---
+++ src/DesiredTemp.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<socket.h>
+
+void main()
+{
+	int socket_temp;
+
+    while(1)
+    {
+		double d;
+		char temp[20];
+		char input[20];
+
+		printf("\nEnter desired temp [5.00, 30.00]\n");
+		fgets(input, 20, stdin);
+
+		d = atof(input);
+
+		if(d < 5 || d > 30) {
+			printf("\nTemp outside valid interval\n");
+		} else {
+			sprintf(temp, "%.2f", d);
+			printf("\nSending temp: %s\n", temp);
+
+			// TODO Send the temp
+		}
+	}
+
+	close(socket_temp);
+}
