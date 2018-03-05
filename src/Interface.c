c69c025075e88cf94f335c44ad7295ce35ea9869
Jean Jacques Akoffodji <jean-jacques.akoffodji@polymtl.ca>
2018-03-04 22:54:21 -0500

    Modification de la puissance

--- src/Interface.c
+++ src/Interface.c
@@ -1,12 +1,10 @@
-/*! \file Interface.c */
-
 /**
  * \file Interface.c
  * \author Jean Jacques Akoffodji
  * \date 03 Mars 2018
  * \brief Fichier qui contient la fonction main du programme principale et les fonctions utilisées pour les threads
  */
-
+#include <math.h>
 #include <semaphore.h>
 #include <pthread.h>
 #include <sched.h>
@@ -53,6 +51,27 @@ int socketAffichage;
 
 
 /**
+ * \fn void powerCalculation()
+ * \brief Fonction de calcul de la puissance nécessaire.
+ */
+void powerCalculation()
+{
+    globalPower = ((temperatureDesired-dataBME280.Temp)/6)*100;
+    
+    if (globalPower < 0 )
+    {
+        globalPower = 0;
+    }
+    
+    if (globalPower > 100 )
+    {
+        globalPower = 100;
+    }
+}
+
+
+
+/**
  * \fn void sendTempD (int socket_desc)
  * \brief Fonction d'envoi de la température désirée et de la puissance calculée au programme d'affichage.
  */
@@ -64,14 +83,14 @@ void sendTempD (int socket_desc)
     char message[12];
     
     sprintf(message, "TD%.2f\n",temperatureDesired);
-    puts(message);
+    
     if( send(socket_desc , &message , strlen(message) , 0) < 0)
     {
         puts("Temperature Desired Send failed");
     }
     
     sprintf(message, "PW%.2f\n",globalPower);
-    puts(message);
+
     if( send(socket_desc , &message , strlen(message) , 0) < 0)
     {
         puts("Power Send failed");
@@ -86,10 +105,12 @@ void sendTempD (int socket_desc)
  */
 void* socketBME280(void * x)
 {
-    char message[30];
+
     struct socketInt socketBME;
+    double currentPower;
+    double deltaPower;
     socketBME = configuration_server(4598);
-    
+    char message[12];
     struct sched_param param;
     
     param.sched_priority = 3;
@@ -99,17 +120,30 @@ void* socketBME280(void * x)
     {
         
         sleep(3);
+        
         sem_wait(&mutex);
+        
+        currentPower = globalPower;
+        
         if (recv(socketBME.Client, &dataBME280, sizeof(dataBME280), 0) < 0)
         {
             printf("erreur au recv() data BME280\n");
         }
-        sprintf(message, "Temperature mesured %.2f\n",dataBME280.Temp);
-        printf(message);
-        sprintf(message, "Humidity mesured %.2f\n",dataBME280.Humidity);
-        printf(message);
-        sprintf(message, "Pressure mesured %.2f\n",dataBME280.Pressure);
-        printf(message);
+        
+        powerCalculation();
+        
+        deltaPower = currentPower - globalPower;
+        
+        if (fabs(deltaPower) > 0.05)
+        {
+            
+            sprintf(message, "PW%.2f\n",globalPower);
+            if( send(socketAffichage , &message , strlen(message) , 0) < 0)
+            {
+                puts("Power Send failed");
+            }
+        }
+
         sem_post(&mutex);
     }
 }
@@ -133,7 +167,7 @@ void* affichageBME280(void * x)
         sleep(3);
         sem_wait(&mutex);
         sprintf(message, "TP%.2f\n",dataBME280.Temp);
-        puts(message);
+        
         if( send(socketAffichage , &message , strlen(message) , 0) < 0)
         {
             puts("Temperature ambiante Send failed");
@@ -176,7 +210,7 @@ void* affichageTempDesired(void * x)
         sem_wait(&mutex);
         
         sprintf(message, "TD%.2f\n",temperatureDesired);
-        puts(message);
+        
         if( send(socketAffichage , &message , strlen(message) , 0) < 0)
         {
             puts("Temperature ambiante Send failed");
@@ -189,24 +223,6 @@ void* affichageTempDesired(void * x)
 
 
 
-/**
- * \fn void powerCalculation()
- * \brief Fonction de calcul de la puissance nécessaire.
- */
-void powerCalculation()
-{
-    globalPower = ((temperatureDesired-dataBME280.Temp)/6)*100;
-    
-    if (globalPower < 0 )
-    {
-        globalPower = 0;
-    }
-    
-    if (globalPower > 100 )
-    {
-        globalPower = 100;
-    }
-}
 
 
 /**
@@ -215,7 +231,7 @@ void powerCalculation()
  */
 void* socketTempDesired(void * x)
 {
-    char message[30];
+    
     struct socketInt socketTemp;
     double oneTemp;
     socketTemp = configuration_server(4599);
@@ -233,13 +249,10 @@ void* socketTempDesired(void * x)
         {
             printf("erreur au recv() data Temp desired\n");
         }
-        sprintf(message, "Temperature desired %.2f\n",oneTemp);
-    
-        printf(message);
         
         sem_wait(&mutex);
         
-        temperatureDesired =oneTemp;
+        temperatureDesired = oneTemp;
         powerCalculation();
         sendTempD(socketAffichage);
         
@@ -250,16 +263,6 @@ void* socketTempDesired(void * x)
 
 void main()
 {
-    
-    struct charValue oneData;
-    int Server, Client;
-    struct socketInt socketTemp, socketBME;
-    double temp_desired;
-    double powerNeed;
-    char message[50];
-    struct charValue val;
-    
-
     
     pthread_t threadBME;
     pthread_t threadTempD;
